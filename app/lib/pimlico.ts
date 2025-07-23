a4f90a4c186def55b2ba53a8dce5b4eaf140cfd3
Stoneybro <zionlivingstone4@gmail.com>
2025-07-23 21:06:13 +0100

    moved pimlico key to .env file

--- app/lib/pimlico.ts
+++ app/lib/pimlico.ts
@@ -3,7 +3,7 @@ import { createPublicClient, createWalletClient, http } from 'viem';
 import { baseSepolia } from 'viem/chains';
 import { entryPoint06Address } from "viem/account-abstraction"
 
-export const pimlicoBundlerUrl = "https://api.pimlico.io/v2/84532/rpc?apikey=pim_hTry94xL89FG7zGStsZTca";
+export const pimlicoBundlerUrl = "https://api.pimlico.io/v2/84532/rpc?apikey=${process.env.PIMLICO_API_KEY}";
 export const pimlicoBundlerTransport = http(pimlicoBundlerUrl);
 
 export const pimlicoClient = createPimlicoClient({ 
