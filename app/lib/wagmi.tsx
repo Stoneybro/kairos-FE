4a85d0f1f5511419474e984c7efbdcfbc611356f
Stoneybro <zionlivingstone4@gmail.com>
2025-07-24 13:23:25 +0100

    added alchemy .env

--- app/lib/wagmi.tsx
+++ app/lib/wagmi.tsx
@@ -1,12 +1,11 @@
 import {createConfig} from '@privy-io/wagmi';
 import{http} from "viem";
-import { baseSepolia,anvil } from 'wagmi/chains'
+import { baseSepolia} from 'wagmi/chains'
 
 export const config = createConfig({
-  chains: [baseSepolia,anvil],
+  chains: [baseSepolia],
   ssr: false,
   transports: {
-     [baseSepolia.id]: http('https://base-sepolia.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY'),
-    [anvil.id]:http("http://localhost:8545")
+     [baseSepolia.id]: http(`https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`),
   },
 })
