7815dbf0784eb7f70bfc7a2cdfa21d2ef121b490
Stoneybro <zionlivingstone4@gmail.com>
2025-07-27 19:18:31 +0100

    removed connect wallet login option

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -32,31 +32,7 @@ export function LoginForm({
           <Mail />
           Login with Mail
         </Button>
-        <div className='after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t'>
-          <span className='bg-background text-muted-foreground relative z-10 px-2'>
-            Or continue with
-          </span>
-        </div>
-        <Button
-          variant='outline'
-          className='w-full flex items-center'
-          disabled={disableLogin}
-          onClick={() =>
-            login({
-              loginMethods: ["wallet"],
-              walletChainType: "ethereum-only",
-            })
-          }
-        >
-          <Wallet />
-          Connect Wallet
-        </Button>
-      </div>
-      <div className='text-center text-sm'>
-        Don&apos;t have an account?{" "}
-        <a href='#' className='underline underline-offset-4'>
-          Sign up
-        </a>
+
       </div>
     </div>
   );
